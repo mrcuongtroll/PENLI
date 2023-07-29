1134f1e74da032f94f6833e1c8bf7131de001376
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2023-07-29 17:10:14 +0700

    added run scripts

---
+++ shell_scripts/baselines/run_bert_baseline.sh
@@ -0,0 +1,21 @@
+python train_baseline.py \
+    --device=cuda \
+    --config=./configs/baseline_bert.json
+
+python test_baseline.py \
+    --device=cuda \
+    --config=./configs/baseline_bert.json \
+    --best_ckpt \
+    --test_dataset_path=./datasets/e-SNLI/esnli_test.csv
+
+python test_baseline.py \
+    --device=cuda \
+    --config=./configs/baseline_bert.json \
+    --best_ckpt \
+    --test_dataset_path=./datasets/multinli_1.0/multinli_1.0_dev_matched.jsonl
+
+python test_baseline.py \
+    --device=cuda \
+    --config=./configs/baseline_bert.json \
+    --best_ckpt \
+    --test_dataset_path=./datasets/multinli_1.0/multinli_1.0_dev_mismatched.jsonl
---
+++ shell_scripts/prompted/run_bert_prompted.sh
@@ -0,0 +1,21 @@
+python train.py \
+   --device=cuda \
+   --config=./configs/default_mlm.json
+
+python test.py \
+   --device=cuda \
+   --config=./configs/default_mlm.json \
+   --best_ckpt \
+   --test_dataset_path=./datasets/e-SNLI/esnli_test.csv
+
+python test.py \
+   --device=cuda \
+   --config=./configs/default_mlm.json \
+   --best_ckpt \
+   --test_dataset_path=./datasets/multinli_1.0/multinli_1.0_dev_matched.jsonl
+
+python test.py \
+   --device=cuda \
+   --config=./configs/default_mlm.json \
+   --best_ckpt \
+   --test_dataset_path=./datasets/multinli_1.0/multinli_1.0_dev_mismatched.jsonl
